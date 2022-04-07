5ea12c5d97ff6bed60a940f8b26b5d36f7adbee5
sublimesaurabh <99521310+sublimesaurabh@users.noreply.github.com>
2022-04-07 10:44:45 -0400

    Added TODO, a couple bugs need fixing

--- src/StudentInformation.java
+++ src/StudentInformation.java
@@ -1,3 +1,6 @@
+// Made: April 6, 2022.
+// By Saurabh Chapagain
+// s205@umbc.edu
 import java.util.Scanner;
 
 public class StudentInformation {
@@ -10,16 +13,25 @@ public class StudentInformation {
    private String gradeYear;
    private int courseCost = 1104;
 
+   /*TODO: 4/7/22
+    Course Cost needs to be expanded upon
+     - Full time and Part time students are charged different
+     - Limit students to only enroll in 7 courses, if they try to enroll more
+     they need permission from advisor
+     - In State students are charged differently, than out-of-state students
+    */
+
    public void Student() {
        Scanner userInput = new Scanner(System.in);
-       System.out.println("Please enter your name: ");
+       System.out.println("Please enter your first name: ");
        this.firstName = userInput.nextLine();
-       System.out.println("Please enter your name: ");
+       System.out.println("Please enter your last name: ");
        this.lastName = userInput.nextLine();
        System.out.println("Hello, " + this.firstName + this.lastName);
        System.out.println("Please enter your UMBC email: ");
        this.studentEmail = userInput.nextLine();
    }
+
 
    public void enroll() {
        do {
@@ -41,6 +53,5 @@ public class StudentInformation {
        System.out.println("ENROLLED IN: " + courseName);
        System.out.println("TUITION BALANCE: " + totalBalance);
    }
-
 }
 
