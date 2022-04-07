a13f4f842517c7e43326cd4f482a6b86bf444161
sublimesaurabh <99521310+sublimesaurabh@users.noreply.github.com>
2022-04-06 20:07:38 -0400

    swag

---
+++ src/RetriverReady.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class RetriverReady {
+    public static void main(String[] args) {
+        //getUserInfo();
+        StudentInformation student = new StudentInformation();
+        student.Student();
+        student.enroll();
+    }
+}
+
