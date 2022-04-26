f9ec83c4ab0ba84a9e31555e07b07206540488a6
sublimesaurabh <99521310+sublimesaurabh@users.noreply.github.com>
2022-04-25 23:55:01 -0400

    expanded on JavaMail

--- src/JavaMail.java
+++ src/JavaMail.java
@@ -1,12 +1,54 @@
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.*;
 import javax.mail.internet.*;
+import javax.mail.Message;
 import javax.activation.*;
+import javax.mail.internet.MimeMessage;
 
-public class JavaMail {
+public class JavaMail extends StudentInformation {
 
-//    public static void sendMail(String to, String subject, String body) {
-//        // Recipient's email ID needs to be mentioned.
+    public static void sendMail(String recipient) throws MessagingException {
+        System.out.println("Sending mail to " + recipient);
+
+        // Get system properties
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "587");
+
+        String myEmail = "ouremail@gmail.com";
+        String myPassword = "password";
+
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("myEmail", "myPassword");
+            }
+        });
+//TODO: fix bug, in message class....
+//        Message message = new prepareMessage(session, myEmail, recipient);
+//        Transport.send(message);
+        System.out.println("Sent message successfully....");
+    }
+
+    private static Message prepareMessage (Session session, String recipient, String subject, String body) throws MessagingException {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("myEmail"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+            message.setSubject(subject);
+            message.setText(body);
+            return message;
+        } catch (MessagingException e) {
+            Logger.getLogger(JavaMail.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+
+        //        // Recipient's email ID needs to be mentioned.
 //        String to = "abcd@gmail.com";
 //
 //        // Sender's email ID needs to be mentioned
@@ -66,6 +108,8 @@ public class JavaMail {
 //            //asdbckjsdbckjasdck
 //        } catch (MessagingException e) {
 //        }
-//    }
+        return null;
+    }
+
 }
 
